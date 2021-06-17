252c6e4189f635b566b7287587df20d3f1a67379
Hüseyin Bera Bulut <berabulut@gmail.com>
2021-06-17 19:34:01 +0300

    added original link's qrcode

--- pages/qrcode/[key].js
+++ pages/qrcode/[key].js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
+import { getStats } from "api";
 import { useRouter } from "next/router";
 import { makeStyles } from "@material-ui/core/styles";
-import { Button } from "@material-ui/core";
+import { Button, Paper, Tabs, Tab, Box } from "@material-ui/core";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 import QRCode from "qrcode.react";
 
@@ -13,15 +14,63 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     flexDirection: "column",
   },
-  qrcode: {
-    marginTop: "128px",
+  paper: {
+    [theme.breakpoints.up("lg")]: {
+      width: "25%",
+    },
+    [theme.breakpoints.down("lg")]: {
+      width: "30%",
+    },
+    [theme.breakpoints.down("md")]: {
+      width: "40%",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "55%",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "80%",
+    },
   },
   downloadButton: {
     marginTop: "50px",
     color: "white",
     backgroundColor: "#00ADB5",
   },
+  title: {
+    marginTop: "96px",
+    marginBottom: "32px",
+    [theme.breakpoints.down("xs")]: {
+      marginTop: "64px",
+    },
+  },
 }));
+
+function TabPanel(props) {
+  const { children, value, index, url, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <QRCode value={url} style={{ height: "156px", width: "156px" }} />
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 const QR = () => {
   const router = useRouter();
@@ -29,6 +78,13 @@ const QR = () => {
   const { key } = router.query;
 
   const [url, setUrl] = useState(" ");
+  const [realURL, setRealURL] = useState("");
+  const [title, setTitle] = useState("");
+  const [tab, setTab] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setTab(newValue);
+  };
 
   const download = () => {
     /// create an "off-screen" anchor tag
@@ -83,13 +139,32 @@ const QR = () => {
     setUrl(process.env.redirectingURL + "/" + key);
   }, []);
 
+  useEffect(() => {
+    async function fetchURL() {
+      const response = await getStats(process.env.apiURL + "/" + key);
+      setRealURL(response.record.Value);
+      setTitle(response.record.Title);
+    }
+    fetchURL();
+  }, []);
+
   return (
     <div className={classes.container}>
-      <QRCode
-        className={classes.qrcode}
-        value={url}
-        style={{ height: "156px", width: "156px" }}
-      />
+      <h2 className={classes.title}>{title}</h2>
+      <Paper className={classes.paper} square>
+        <Tabs
+          value={tab}
+          onChange={handleChange}
+          aria-label="simple tabs example"
+          centered
+          variant="fullWidth"
+        >
+          <Tab label="ORIGINAL" {...a11yProps(0)} />
+          <Tab label="SHORTENED" {...a11yProps(1)} />
+        </Tabs>
+      </Paper>
+      <TabPanel value={tab} url={realURL} index={0} />
+      <TabPanel value={tab} url={url} index={1} />
       <Button
         variant="contained"
         size="medium"
